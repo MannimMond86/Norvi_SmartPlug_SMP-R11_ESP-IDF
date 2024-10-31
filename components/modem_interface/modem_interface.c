c27f45f486aff4af64d953c6419c2abc846c838a
TjarkZiehm <tjark@ziehmconsulting.com>
2024-10-31 12:47:41 +0100

    add for main and components file header documentation

--- components/modem_interface/modem_interface.c
+++ components/modem_interface/modem_interface.c
@@ -1,3 +1,30 @@
+/***********************************************************************************************
+* FILENAME       : modem_interface.c
+ * VERSION        : 0.0.1
+ * DESCRIPTION    :
+ *
+ * PUBLIC FUNCTIONS:
+ *    - func()
+ *
+ * NOTES:
+ *    Prefixes denote variable or return types:
+ *    - "c" for char, "s" for short, "v" for void, "u" for unsigned, and "x" for non-standard types.
+ *
+ * AUTHOR         : Tjark Ziehm
+ * START DATE     : October 1, 2024
+ * COPYRIGHT      : © 2024 Tjark Ziehm
+ * LICENCE        : CC BY-NC-ND 4.0
+ * VERSION FORMAT : <major>.<minor>.<patch>
+ *
+ * Contributors   :
+ *
+ * CHANGE LOG:
+ *    - v0.0.1:
+ *    - v0.0.2:
+ *    - v0.1.0:
+ *    - v1.0.0:
+ ***********************************************************************************************/
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include <stdio.h>
